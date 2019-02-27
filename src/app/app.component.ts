6cc9e345dd545c563e28f0829480adb998996daa
Zou, QianLing <qianling.zou@siemens.com>
2019-02-27 16:18:57 +0800

    更改对外暴露的model和事件传递的方式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit} from '@angular/core';
-import { DynamicFormComponent,
-         SingleSelectModel,
-         MultiSelectModel,
-         InputLabelModel,
-         LabelModel,
-         Direction } from './dynamic-form/dynamic-form.component';
+import { Component, OnInit } from '@angular/core';
+import {
+  DynamicFormComponent,
+  ConfigFormData,
+  ControlType,
+  Direction
+} from './dynamic-form/dynamic-form.component';
+import { Observable, of } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,125 +13,221 @@ import { DynamicFormComponent,
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  formModel: Array<any>;
+  formDataModel: Array<ConfigFormData>;
   constructor() { }
 
   ngOnInit() {
-    const inputModel: InputLabelModel = {
-      id: 'dataSource',
-      type: 'text',
-      label: 'DataSources',
-      placeholder: 'pls input here',
-      value: 'initial text',
-      handler: ((a, b) => {
-        this.log(a, b);
-      }),
-      style: {
-        fxLayout: Direction.row,
-        fxLayoutGap: '20px',
-        fxLayoutAlign: 'center center',
-        height: '100px',
-        margin: '20px 0 0 0'
-      }
-    };
-    const singleModel: SingleSelectModel = {
-      id: 'interval',
-      type: 'radio',
-      label: 'Interval',
-      options: [
-        {
-          label: 'week',
-          value: 'week',
-        },
-        {
-          label: 'day',
-          value: 'day',
-        },
-        {
-          label: 'hour',
-          value: 'hour',
-        },
-      ],
-      selectedOption: 'hour',
-      handler: ((a, b) => {
-        this.log(a, b);
-      })
-    };
-    const dropDownModel: SingleSelectModel = {
-      id: 'dataTypes',
-      type: 'dropDown',
-      label: 'DataTypes',
-      options: [
-        {
-          label: 'raw',
-          value: 'raw',
-        },
-        {
-          label: 'product-line',
-          value: 'product-line',
-        },
-      ],
-      selectedOption: '',
-      handler: ((a, b) => {
-        this.log(a, b);
-      })
-    };
-    const multiModel: MultiSelectModel = {
-      id: 'points',
-      type: 'checkbox',
-      label: 'Points',
-      options: [
-        {
-          id: 'A1Current',
-          value: 'A1Current',
-          checked: true
-        },
-        {
-          id: 'A2Current',
-          value: 'A2Current',
-          checked: true,
-        },
-        {
-          id: 'A3Current',
-          value: 'A3Current',
-          checked: false,
+    this.formDataModel = [
+      {
+        id: 'componentName',
+        type: ControlType.input,
+        name: 'componentName',
+        value: 'initial text',
+        placeholder: 'pls input here',
+        handler$: (res: any) => {
+          return Observable.create(observer => {
+            const temp = res;
+            observer.next(temp);
+          });
         }
-      ],
-      handler: ((a, b) => {
-        this.log(a, b);
-      })
-    };
-    const colorPickerModel: MultiSelectModel = {
-      id: 'pointcolor',
-      type: 'colorPicker',
-      label: 'Points Colors',
-      options: [
-        {
-          id: 'A1Current',
-          label: 'A1Current',
-          value: '#ff0000',
-          checked: true
-        },
-        {
-          id: 'A2Current',
-          label: 'A2Current',
-          value: '#00ff00',
-          checked: true,
-        },
-        {
-          id: 'A3Current',
-          label: 'A3Current',
-          value: '#0000ff',
-          checked: false,
+      },
+      {
+        id: 'dataSource',
+        type: ControlType.dropDown,
+        name: 'Data Sources',
+        value: '',
+        enum: ['MHOP50.1', 'MHOP50.3', 'MHOP50.4', 'MHOP50.5'],
+        handler$: (res: any) => {
+          return Observable.create(observer => {
+            const temp = res;
+            observer.next(temp);
+          });
         }
-      ],
-      handler: ((a, b) => {
-        this.log(a, b);
-      })
-    };
-    this.formModel = [inputModel, dropDownModel, multiModel, colorPickerModel, singleModel];
+      },
+      {
+        id: 'dataType',
+        type: ControlType.dropDown,
+        name: 'Data Types',
+        value: '',
+        enum: ['raw', 'product-line'],
+        handler$: (res: any) => {
+          return Observable.create(observer => {
+            const temp = res;
+            observer.next(temp);
+          });
+        }
+      },
+      {
+        id: 'dataPoints',
+        type: ControlType.checkbox,
+        name: 'Data Points',
+        value: '',
+        enum: [{id: 'A1Current', name: 'A1Current', value: true},
+               {id: 'A2Current', name: 'A2Current', value: false},
+               {id: 'A3Current', name: 'A3Current', value: true},
+               {id: 'A4Current', name: 'A4Current', value: false},
+               {id: 'A5Current', name: 'A5Current', value: true}],
+        handler$: (res: any) => {
+          return Observable.create(observer => {
+            const temp = res;
+            observer.next(temp);
+          });
+        }
+      },
+      {
+        id: 'PointsColor',
+        type: ControlType.colorPicker,
+        name: 'Points Color',
+        value: '',
+        enum: [{id: 'A1Current', name: 'A1Current', value: '#333333'},
+               {id: 'A2Current', name: 'A2Current', value: '#0000ff'},
+               {id: 'A3Current', name: 'A3Current', value: '#ff00ff'},
+               {id: 'A4Current', name: 'A4Current', value: '#ff0000'},
+               {id: 'A5Current', name: 'A5Current', value: '#00ff00'}],
+        handler$: (res: any) => {
+          return Observable.create(observer => {
+            const temp = res;
+            observer.next(temp);
+          });
+        }
+      },
+      {
+        id: 'interval',
+        type: ControlType.radio,
+        name: 'Interval',
+        value: 'minute',
+        enum: ['minute', 'hour', 'day'],
+        handler$: (res: any) => {
+          return Observable.create(observer => {
+            const temp = res;
+            observer.next(temp);
+          });
+        }
+      },
+    ];
+    //   controlModel.handler$(form.value).subscribe(
+    //     (value) => {
+    //       form =
+    //     }
+    //   );
+    // const inputModel: InputLabelModel = {
+    //   id: 'dataSource',
+    //   type: 'text',
+    //   label: 'DataSources',
+    //   placeholder: 'pls input here',
+    //   value: 'initial text',
+    //   handler: ((a, b) => {
+    //     this.log(a, b);
+    //   }),
+    //   style: {
+    //     fxLayout: Direction.row,
+    //     fxLayoutGap: '20px',
+    //     fxLayoutAlign: 'center center',
+    //     height: '100px',
+    //     margin: '20px 0 0 0'
+    //   }
+    // };
+    // const singleModel: SingleSelectModel = {
+    //   id: 'interval',
+    //   type: 'radio',
+    //   label: 'Interval',
+    //   options: [
+    //     {
+    //       label: 'week',
+    //       value: 'week',
+    //     },
+    //     {
+    //       label: 'day',
+    //       value: 'day',
+    //     },
+    //     {
+    //       label: 'hour',
+    //       value: 'hour',
+    //     },
+    //   ],
+    //   selectedOption: 'hour',
+    //   handler: ((a, b) => {
+    //     this.log(a, b);
+    //   })
+    // };
+    // const dropDownModel: SingleSelectModel = {
+    //   id: 'dataTypes',
+    //   type: 'dropDown',
+    //   label: 'DataTypes',
+    //   options: [
+    //     {
+    //       label: 'raw',
+    //       value: 'raw',
+    //     },
+    //     {
+    //       label: 'product-line',
+    //       value: 'product-line',
+    //     },
+    //   ],
+    //   selectedOption: '',
+    //   handler: ((a, b) => {
+    //     this.log(a, b);
+    //   })
+    // };
+    // const multiModel: MultiSelectModel = {
+    //   id: 'points',
+    //   type: 'checkbox',
+    //   label: 'Points',
+    //   options: [
+    //     {
+    //       id: 'A1Current',
+    //       value: 'A1Current',
+    //       checked: true
+    //     },
+    //     {
+    //       id: 'A2Current',
+    //       value: 'A2Current',
+    //       checked: true,
+    //     },
+    //     {
+    //       id: 'A3Current',
+    //       value: 'A3Current',
+    //       checked: false,
+    //     }
+    //   ],
+    //   handler: ((a, b) => {
+    //     this.log(a, b);
+    //   })
+    // };
+    // const colorPickerModel: MultiSelectModel = {
+    //   id: 'pointcolor',
+    //   type: 'colorPicker',
+    //   label: 'Points Colors',
+    //   options: [
+    //     {
+    //       id: 'A1Current',
+    //       label: 'A1Current',
+    //       value: '#ff0000',
+    //       checked: true
+    //     },
+    //     {
+    //       id: 'A2Current',
+    //       label: 'A2Current',
+    //       value: '#00ff00',
+    //       checked: true,
+    //     },
+    //     {
+    //       id: 'A3Current',
+    //       label: 'A3Current',
+    //       value: '#0000ff',
+    //       checked: false,
+    //     }
+    //   ],
+    //   handler: ((a, b) => {
+    //     this.log(a, b);
+    //   })
+    // };
+    // this.formModel = [inputModel, dropDownModel, multiModel, colorPickerModel, singleModel];
   }
 
+  log2(formValue): Observable<any> {
+
+  }
   log(message, b) {
     console.log(message, b);
   }
--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
 import { stringify } from '@angular/core/src/util';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { type } from 'os';
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
@@ -10,49 +11,15 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 })
 export class DynamicFormComponent implements OnInit {
   form: FormGroup;
+  formModels = [];
   @Input('model')
   get model() {
     return this._model;
   }
   set model(value) {
     this._model = value;
-    const map = {};
-    for (const element of this.model) {
-      switch (element.type) {
-        case 'radio':
-        case 'dropDown':
-          {
-            map[element.id] = new FormControl(element.selectedOption);
-          }
-          break;
-        case 'text':
-          {
-            map[element.id] = new FormControl(element.value);
-          }
-          break;
-        case 'checkbox':
-          {
-            const subMap = {};
-            for (const iterator of element.options) {
-              subMap[iterator.id] = new FormControl(iterator.checked);
-            }
-            map[element.id] = new FormGroup(subMap);
-          }
-          break;
-        case 'colorPicker':
-          {
-            const subMap = {};
-            for (const iterator of element.options) {
-              subMap[iterator.id] = new FormControl(iterator.value);
-            }
-            map[element.id] = new FormGroup(subMap);
-          }
-          break;
-        default:
-          break;
-      }
-    }
-    this.form = new FormGroup(map);
+    this.formModels = [];
+    this.createFormModel(this._model);
     console.log(this.form.value);
   }
   _model: Array<any>;
@@ -65,76 +32,138 @@ export class DynamicFormComponent implements OnInit {
     console.log(this.form.value);
   }
 
-  // onRadioChange(event, controlModel, form) {
-  //   controlModel.handler$(form.value).subscribe(
-  //     (value) => {
-  //       form =
-  //     }
-  //   );
-  // }
+  onRadioChange(event, controlModel, form) {
+    controlModel.handler$(form.value).subscribe(
+      (value) => {
+        console.log(value);
+      }
+    );
+  }
   attributeDesciption(data): string {
     console.log(JSON.stringify(data));
     // return data.stringify;
     return JSON.stringify(data);
   }
+  private createFormModel(formDataSet: ConfigFormData[]) {
+    const map = {};
+    for (let index = 0; index < formDataSet.length; index++) {
+      const formData = formDataSet[index];
+      switch (formData.type) {
+        case ControlType.input:
+          {
+            map[formData.id] = new FormControl(formData.value);
+            const model = new InputLabelModel(formData);
+            this.formModels.push(model);
+          }
+          break;
+        case ControlType.radio:
+        case ControlType.dropDown:
+          {
+            map[formData.id] = new FormControl(formData.value);
+            const model = new SingleSelectModel(formData);
+            this.formModels.push(model);
+          }
+          break;
+        case ControlType.checkbox:
+        case ControlType.colorPicker:
+          {
+            const subMap = {};
+            for (const element of formData.enum) {
+              const option = element as { id: string, name: string, value?: any };
+              subMap[option.id] = new FormControl(option.value);
+            }
+            map[formData.id] = new FormGroup(subMap);
+            const model = new MultiSelectModel(formData);
+            this.formModels.push(model);
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    this.form = new FormGroup(map);
+  }
 }
 
-export interface SingleSelectModel extends BasicModel {
+
+class BasicModel {
+  constructor(config) {
+    this.id = config.id;
+    this.type = config.type;
+    this.name = config.name;
+    this.value = config.value;
+    this.handler$ = config.handler$;
+    this.style = config.style;
+  }
+  id: string;
+  type: ControlType;
+  name?: string;
+  value?: any;
+  handler$?: () => Observable<any>;
+  style?: BasicStyleModel;
+}
+class SingleSelectModel extends BasicModel {
+  constructor(config: ConfigFormData) {
+    super(config);
+    const options = [];
+    for (const value of config.enum) {
+      options.push({
+        name: value,
+        value: value
+      });
+    }
+    this.options = options;
+    this.selectedOption = config.value;
+  }
   options: Array<{
-    label: string,
+    name: string,
     value: string,
   }>;
   selectedOption: string;
 }
-export interface MultiSelectModel extends BasicModel {
+class MultiSelectModel extends BasicModel {
+  constructor(config: ConfigFormData) {
+    super(config);
+    const options = [];
+    for (const value of config.enum) {
+      const option = value as { id: string, name?: string, value?: any };
+      options.push({
+        id: option.id,
+        name: option.name,
+        value: option.value,
+      });
+    }
+    this.options = options;
+  }
   options: Array<{
     id: string,
-    value: string,
-    label?: string,
-    checked: boolean,
+    name?: string,
+    value: boolean,
   }>;
 }
-export interface InputLabelModel extends BasicModel {
+class InputLabelModel extends BasicModel {
+  constructor(config: ConfigFormData) {
+    super(config);
+    this.placeholder = config.placeholder ? config.placeholder : '';
+  }
   placeholder: string;
-  value: string;
 }
-export interface LabelModel {
+interface LabelModel {
   id: string;
   type: string;
 }
-export interface BasicModel {
-  id: string;
-  type: string;
-  label?: string;
-  handler?: ((a, b) => void);
-  style?: BasicStyleModel;
-}
-
 export interface ConfigFormData {
   id: string;
   type: string;
   name: string;
-  default?: any;
+  value?: any;
   validation?: string; // regex string
-  handler$?: () => Observable<any>;
-  enum?: string[];
+  handler$?: (data) => Observable<any>;
+  enum?: { id: string, name?: string, value?: any }[] | string[];
   child?: ConfigFormData[];
+  placeholder?: string;
 }
 
-// function getFormModel(formDataSet: ConfigFormData[]) {
-//   for (let index = 0; index < formDataSet.length; index++) {
-//     const element = formDataSet[index];
-//     switch () {
-//       case value:
-
-//         break;
-
-//       default:
-//         break;
-//     }
-
-//   }
-// }
 
 export interface BasicStyleModel {
   fxLayoutGap?: string;
